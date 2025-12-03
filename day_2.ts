c016abdcf215a075b4e925cee27e80e30acc26d2
Willem Serruys <willem.serruys@external.atlascopco.com>
2025-12-03 20:44:18 +0100

    chore: cleanup day2

--- day_2.ts
+++ day_2.ts
@@ -2,15 +2,18 @@ let input = (await Bun.file("./inputs/day2_input.txt").text())
 	.split("\n")
 	.flatMap((i) => i.split(",").filter((x) => x != ""));
 
-let result = input.reduce((acc, curr) => {
-	var [lowerLimit, upperLimit] = GetRowItem(curr);
-	for (const number of FindInvalidIds(lowerLimit, upperLimit)) {
-		acc += number;
-	}
-	return acc;
-}, 0);
+console.log(Result(true));
+console.log(Result(false));
 
-console.log(result);
+function Result(isPart1: boolean): number {
+	return input.reduce((acc, curr) => {
+		var [lowerLimit, upperLimit] = GetRowItem(curr);
+		for (const number of FindInvalidIds(lowerLimit, upperLimit, isPart1)) {
+			acc += number;
+		}
+		return acc;
+	}, 0);
+}
 
 function GetRowItem(input: String): [number, number] {
 	const regex = /\d+/g;
@@ -18,35 +21,20 @@ function GetRowItem(input: String): [number, number] {
 	return [parseInt(items[0]!), parseInt(items[1]!)];
 }
 
-function FindInvalidIds(lowerLimit: number, upperLimit: number): number[] {
+function FindInvalidIds(lowerLimit: number, upperLimit: number, isPart1: boolean): number[] {
 	let result = [];
 	for (let i = lowerLimit; i <= upperLimit; i++) {
-		if (IsInvalidIdPart2(i)) {
+		if (IsInvalidId(i, isPart1)) {
 			result.push(i);
 		}
 	}
 	return result;
 }
 
-function IsInvalidId(input: number): boolean {
+function IsInvalidId(input: number, isPart1: boolean): boolean {
 	let inputString = input.toString();
-	if (inputString.length % 2 != 0) return false;
-	let splitId = [
-		inputString.substring(0, inputString.length / 2),
-		inputString.substring(inputString.length / 2, inputString.length),
-	];
-	let result = splitId[0] === splitId[1];
-	if (result) console.log(`${input} = ${splitId}`);
-	return result;
-}
-
-function IsInvalidIdPart2(input: number): boolean {
-	let inputString = input.toString();
-	const regex = /(^\d*)(\1+)$/gm;
+	const regex = isPart1 ? /(^\d*)(\1)$/gm : /(^\d*)(\1+)$/gm;
 	let items = inputString.match(regex);
 	let result = (items != null && items[0].toString() === inputString);
-	if (result) {
-		console.log(`${items} - ${result} - ${input}`);
-	}
 	return result;
 }
