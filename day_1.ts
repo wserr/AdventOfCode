a722266f09bddf36b1f11ca1083f0abfefab00b7
Willem Serruys <willem.serruys@external.atlascopco.com>
2025-12-03 20:36:44 +0100

    refactored day1

--- day_1.ts
+++ day_1.ts
@@ -1,17 +1,26 @@
 let input = (await Bun.file("./inputs/day1_input.txt").text()).split("\n").filter(x => x != "");
-let counter = 0;
-input.reduce((acc, current) => {
-	let [turns, left] = ParseInput(current);
-	let [newResult, zeroPassages] = Rotate(acc, turns, left);
-	counter += zeroPassages;
-	return newResult;
-}, 50);
-console.log(counter);
+
+console.log(Result(true));
+console.log(Result(false));
+
+function Result(isPart1: boolean): number {
+	let counter = 0;
+	let previousValue = 50;
+	input.forEach((i) => {
+		let [turns, left] = ParseInput(i);
+		let [newResult, zeroPassages] = Rotate(previousValue, turns, left, isPart1);
+		counter += zeroPassages;
+		previousValue = newResult;
+	})
+	return counter;
+}
+
 function ParseInput(element: String): [number, boolean] {
 	let number = parseInt(element.match("\\d+")![0]);
 	return [number, element[0] === 'L'];
 }
-function Rotate(currentPosition: number, turns: number, left: boolean): [number, number] {
+
+function Rotate(currentPosition: number, turns: number, left: boolean, isPart1: boolean): [number, number] {
 	let newPosition = currentPosition;
 	let zeroPassages = 0;
 	let multiplier = left ? -1 : 1;
@@ -19,7 +28,8 @@ function Rotate(currentPosition: number, turns: number, left: boolean): [number,
 		newPosition += multiplier;
 		if (newPosition < 0) newPosition = 99;
 		if (newPosition > 99) newPosition = 0;
-		if (newPosition == 0) zeroPassages++;
+		if (newPosition == 0 && !isPart1) zeroPassages++;
 	}
+	if (newPosition === 0 && isPart1) zeroPassages++;
 	return [newPosition, zeroPassages];
 }
